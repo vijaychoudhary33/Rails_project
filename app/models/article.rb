59998324034e5cc2cad1a29e4846968de96e58ba
vijay <vijaychoudharyin@gmail.com>
2023-05-17 11:25:53 +0530

    Basic Authentication and Deleting Comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,30 +1,8 @@
-# class Article < ApplicationRecord
-#   has_many :comments
-
-#   validates :title, presence: true
-#   validates :body, presence: true, length: { minimum: 10 }
-# end
-# class Article < ApplicationRecord
-#   has_many :comments
-
-#   validates :title, presence: true
-#   validates :body, presence: true, length: { minimum: 10 }
-
-#   VALID_STATUSES = ['public', 'private', 'archived']
-
-#   validates :status, inclusion: { in: VALID_STATUSES }
-
-#   def archived?
-#     status == 'archived'
-#   end
-# end
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
-
-
